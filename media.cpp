4a8c0a890dcd6151cefac79725ec311ae872380a
paixaoS2 <103532904+paixaoS2@users.noreply.github.com>
2022-09-05 16:42:24 -0300

    Create media.cpp

---
+++ media.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (){
+
+  float p1,p2,mf;
+  printf("Digite a P1.: ");
+  scanf("%f",&p1);
+  printf("Digite a P2.: ");
+  scanf("%f",&p2);
+  md=(p1+p2)/2;
+  printf("A media eh.: %.2f",mf);
+
+  return 0;
+}
